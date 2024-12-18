886da280435c1adef7cda68e3aa2fcd99e12c019
Vadim Zhuravlev <vadim.zhuravlev.cranewf@gmail.com>
2024-12-18 16:21:51 +0300

    Update gh.test and jest.config

--- puppeteer/gh.test.js
+++ puppeteer/gh.test.js
@@ -8,69 +8,68 @@ afterAll(async () => {
   await global.browser.close();
 });
 
-describe("Github team page tests", () => {
+describe("Github tests", () => {
   beforeEach(async () => {
     page = await global.browser.newPage();
-    await page.goto("https://github.com/team");
   });
 
   afterEach(async () => {
     await page.close();
   });
 
-  test("The h1 header content", async () => {
-    const firstLink = await page.$("header div div a");
-    await firstLink.click();
-    await page.waitForSelector("h1", { timeout: 5000 });
-    const title2 = await page.title();
-    expect(title2).toEqual("GitHub: Where the world builds software · GitHub");
-  }, 10000);
-
-  test("The first link attribute", async () => {
-    const actual = await page.$eval("a", (link) => link.getAttribute("href"));
-    expect(actual).toEqual("#start-of-content");
-  }, 5000);
-
-  test("The page contains Sign in button", async () => {
-    const btnSelector = ".btn-large-mktg.btn-mktg";
-
-    await page.waitForSelector(btnSelector, {
-      visible: true,
-      timeout: 7000,
+  describe("Github team page tests", () => {
+    beforeEach(async () => {
+      await page.goto("https://github.com/team");
     });
 
-    const actual = await page.$eval(btnSelector, (link) => link.textContent);
-    expect(actual).toContain("Sign up for free");
-  }, 7000);
-});
+    test("The h1 header content", async () => {
+      const firstLink = await page.$("header div div a");
+      await firstLink.click();
+      await page.waitForSelector("h1", { timeout: 5000 });
+      const title2 = await page.title();
+      expect(title2).toEqual(
+        "GitHub: Where the world builds software · GitHub"
+      );
+    }, 10000);
 
-describe("Github other pages tests", () => {
-  beforeEach(async () => {
-    page = await global.browser.newPage();
+    test("The first link attribute", async () => {
+      const actual = await page.$eval("a", (link) => link.getAttribute("href"));
+      expect(actual).toEqual("#start-of-content");
+    }, 5000);
+
+    test("The page contains Sign in button", async () => {
+      const btnSelector = ".btn-large-mktg.btn-mktg";
+
+      await page.waitForSelector(btnSelector, {
+        visible: true,
+        timeout: 7000,
+      });
+
+      const actual = await page.$eval(btnSelector, (link) => link.textContent);
+      expect(actual).toContain("Sign up for free");
+    }, 7000);
   });
 
-  afterEach(async () => {
-    await page.close();
-  });
+  describe("Github other pages tests", () => {
+    test("The h1 header on Features page", async () => {
+      await page.goto("https://github.com/features");
+      await page.waitForSelector("h1", { timeout: 5000 });
+      const header = await page.$eval("h1", (el) => el.textContent.trim());
+      expect(header).toEqual("The tools you need to build what you want.");
+    }, 10000);
 
-  test("The h1 header on Features page", async () => {
-    await page.goto("https://github.com/features");
-    await page.waitForSelector("h1", { timeout: 5000 });
-    const header = await page.$eval("h1", (el) => el.textContent.trim());
-    expect(header).toEqual("The tools you need to build what you want.");
-  });
+    test("The h1 header on Explore page", async () => {
+      await page.goto("https://github.com/explore");
+      await page.waitForSelector("h1", { timeout: 5000 });
+      const header = await page.$eval("h1", (el) => el.textContent.trim());
+      expect(header).toEqual("Explore GitHub");
+    }, 10000);
 
-  test("The h1 header on Explore page", async () => {
-    await page.goto("https://github.com/explore");
-    await page.waitForSelector("h1", { timeout: 5000 });
-    const header = await page.$eval("h1", (el) => el.textContent.trim());
-    expect(header).toEqual("Explore GitHub");
-  });
-
-  test("The h1 header on Pricing page", async () => {
-    await page.goto("https://github.com/pricing");
-    await page.waitForSelector("h1", { timeout: 5000 });
-    const header = await page.$eval("h1", (el) => el.textContent.trim());
-    expect(header).toContain("Get the complete developer platform.");
+    test("The h1 header on Pricing page", async () => {
+      await page.goto("https://github.com/pricing");
+      await page.waitForSelector("h1", { timeout: 5000 });
+      const header = await page.$eval("h1", (el) => el.textContent.trim());
+      expect(header).toContain("Get the complete developer platform.");
+    }, 10000);
   });
 });
